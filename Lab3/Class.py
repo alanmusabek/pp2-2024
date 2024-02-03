75399122701cb009e8c7153ebab465a74175ce6e
Alan M. <waalanmusa@gmail.com>
2024-02-03 17:14:58 +0600

    Add 4 task

--- Lab3/Class.py
+++ Lab3/Class.py
@@ -40,3 +40,30 @@ square.area()
 rectangle = Rectangle(25, 30)
 rectangle.CalculateArea()
 rectangle.area()
+
+# 3 task
+
+class Point():
+    def __init__(self, coordinate_x, coordinate_y):
+        self.coordinate_x = coordinate_x
+        self.coordinate_y = coordinate_y
+        pass
+    def show(self):
+        print(self.coordinate_x, self.coordinate_y)
+    def move(self, coordinate_x, coordinate_y):
+        print("Внесенные изменения: ", self.coordinate_x, self.coordinate_y, " -> ")
+        self.coordinate_x = coordinate_x
+        self.coordinate_y = coordinate_y
+        print(".")
+    def dist(self, first, second):
+        print("Дистанция: ", "(", abs(first.coordinate_x - second.coordinate_x), ",", abs(second.coordinate_y - first.coordinate_y), ")")
+
+first_point = Point(25, -9)
+second_point = Point(-5, -3)
+
+first_point.show()
+second_point.show()
+
+first_point.dist(first_point, second_point)
+
+second_point.dist(second_point, first_point)
