0ec1cf4a634cb75584e6b2513a60e2639825c6d1
Alan M. <waalanmusa@gmail.com>
2024-02-10 17:23:33 +0600

    Add Lab1

---
+++ Lab1/variables.py
@@ -0,0 +1,16 @@
+carname = "Volvo"
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = 10
+z = x + y
+print(z)
+x, y, z = "Orange", "Banana", "Cherry"
+
+x = y = z = "Orange"
+
+def myfunc():
+    global x
+    x = "fantastic"
